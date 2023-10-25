e5a84e82277c5b2b62bb70548de8ae15a0a789e5
Tom Jeannesson <tomjeannesson@gmail.com>
2023-10-25 11:36:01 +0200

    tmp

--- src/Needleman-Wunsch-recmemo.h
+++ src/Needleman-Wunsch-recmemo.h
@@ -1,8 +1,8 @@
 /**
  * \file Needleman-Wunsch-recmemo.h
- * \brief recursive implementation with memoization of Needleman-Wunsch global alignment algorithm that computes the distance between two genetic sequences 
+ * \brief recursive implementation with memoization of Needleman-Wunsch global alignment algorithm that computes the distance between two genetic sequences
  * \version 0.1
- * \date 03/10/2022 
+ * \date 03/10/2022
  * \author Jean-Louis Roch (Ensimag, Grenoble-INP - University Grenoble-Alpes) jean-louis.roch@grenoble-inp.fr
  */
 
@@ -10,23 +10,23 @@
 
 /*
  * Costs for operations on canonical bases
- * Three  operations: insertion and sustitution of one base by an another 
+ * Three  operations: insertion and sustitution of one base by an another
  * Note= substitution of an unknown base N by another one (known or unknown) as the same cost than substitution between 2 different known bases
  */
 /** \def SUBSTITUTION_COST
  *  \brief Cost of substitution of one canonical base by another
  */
-#define SUBSTITUTION_COST	1
+#define SUBSTITUTION_COST 1
 
 /** \def SUBSTITUTION_UNKNOWN_COST
  *  \brief Cost of substitution of an unknown base (N) by another one (canonical or unknown)
  */
-#define SUBSTITUTION_UNKNOWN_COST	1  /* Cost for sustitition of an Unknown bas N by another on -known or unkown- */ 
+#define SUBSTITUTION_UNKNOWN_COST 1 /* Cost for sustitition of an Unknown bas N by another on -known or unkown- */
 
 /** \def INSERTION_COST
- *  \brief Cost of insertion of a canonical base 
+ *  \brief Cost of insertion of a canonical base
  */
-#define INSERTION_COST		2
+#define INSERTION_COST 2
 
 /********************************************************************************
  * Recursive implementation of NeedlemanWunsch with memoization
@@ -34,18 +34,18 @@
 /**
  * \fn long EditDistance_NW_Rec(char* A, size_t lengthA, char* B, size_t lengthB);
  * \brief computes the edit distance between A[0 .. lengthA-1] and B[0 .. lengthB-1]
- * \param A  : array of char represneting a genetic sequence A 
- * \param lengthA :  number of elements in A 
+ * \param A  : array of char represneting a genetic sequence A
+ * \param lengthA :  number of elements in A
  * \param B  : array of char represneting a genetic sequence B
- * \param lengthB :  number of elements in B 
+ * \param lengthB :  number of elements in B
  * \return :  edit distance between A and B }
  *
  * editDistance_RecMemo is a memoized recursive immplementatioin of Needleman-Wunsch algorithm.
  * It allocates the data structure for memoization table and calls the internal recursive function _editDistance_memo
  * that fills in the memoization table.
- * 
+ *
  * If lengthA < lengthB, the sequences A and B are swapped.
  *
  */
-long EditDistance_NW_Rec(char* A, size_t lengthA, char* B, size_t lengthB);
-
+long EditDistance_NW_Rec(char *A, size_t lengthA, char *B, size_t lengthB);
+long EditDistance_NW_Iter(char *A, size_t lengthA, char *B, size_t lengthB);
